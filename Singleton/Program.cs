dd17817b360749d4bdef7182f69bc2c5a62a4ac6
Ujjwal0610 <A00312260@mycambrian.ca>
2025-02-17 22:39:55 +0000

    Initial Commit

---
+++ Singleton/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+public class Singleton
+{
+    
+    private static Singleton _instance;
+
+  
+    private Singleton() { }
+
+   
+    public static Singleton Instance
+    {
+        get
+        {
+           
+            if (_instance == null)
+            {
+                _instance = new Singleton();
+            }
+            return _instance;
+        }
+    }
+
+   
+    public void Log(string message)
+    {
+        Console.WriteLine(message);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       
+        Singleton logger = Singleton.Instance;
+
+       
+        logger.Log("This is a log message.");
+        logger.Log("Singleton pattern ensures only one instance of this class.");
+
+      
+        Singleton anotherLogger = Singleton.Instance;
+        Console.WriteLine($"Is both instances the same? {object.ReferenceEquals(logger, anotherLogger)}");
+
+       
+        Console.ReadLine();
+    }
+}
